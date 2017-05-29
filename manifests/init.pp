76e007ef0f787df091b04538b620f0eb3fb6abdd
Peterclaver Emedom <pemedom1_ms@rocky-pup2k15mas-01.us.umuc.edu>
2017-05-28 23:57:46 -0400

    Modified manifests/init.pp file

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,9 @@ class pm_apache {
                   
 $package = httpd
 $service = httpd
-             
+$file_1  = "/etc/httpd/conf/httpd.conf"
+$file_2  = "/etc/httpd/conf.d/ssl.conf"             
+$file_3  = "/home/pemedom1_ms/file.pc"
 
 if $package == 'present' { 
 
@@ -18,6 +20,15 @@ if $service == 'running' {
 
 service { '$service':
   ensure => running,
+}
+}
+
+if $file_1 == 'present' {
+$one = "Peter IS GOOD"
+$two = "PETER IS MINE"
+$three = "PETER IS OUR LEADER"
+exec { 'echo':                    # exec resource named 'echo'
+  command => '/bin/echo "$one,$two,$three" >> $file_3',
 }
 }
 
