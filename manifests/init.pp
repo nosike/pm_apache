4dcee2caf3b2621e97b003e008a9ce2c6f900a61
Peterclaver Emedom <pemedom1_ms@rocky-pup2k15mas-01.us.umuc.edu>
2017-05-29 02:36:50 -0400

    Modified manifests/init.pp file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,51 +1,45 @@
 #### This module is use to enforce some of the vulnerabilities remediations found in apache servers.
 
 class pm_apache {
-                  
+
 $package = httpd
 $service = httpd
 $file_1  = "/etc/httpd/conf/httpd.conf"
-$file_2  = "/etc/httpd/conf.d/ssl.conf"             
+$file_2  = "/etc/httpd/conf.d/ssl.conf"
 $file_3  = "/home/pemedom1_ms/file.pc"
 
-if $package == 'present' { 
+$file_content = 'one  Peter IS GOOD
+two   PETER IS MINE
+three PETER IS OUR LEADER'
 
-package { '$package':
+if $package == "present" {
+package { "$package":
   ensure => present,
-
 }
-}
-
-if $service == 'running' {
-
-service { '$service':
+service { "$service":
   ensure => running,
 }
 }
 
-if $file_1 == 'present' {
-$one = "Peter IS GOOD"
-$two = "PETER IS MINE"
-$three = "PETER IS OUR LEADER"
-exec { 'echo':                    # exec resource named 'echo'
-  command => '/bin/echo "$one,$two,$three" >> $file_3',
-}
-}
-
-}
-
-
-#class mcelog {
-#$mce_package = mcelog
+#if  $file_3 == 'present' {
+exec { "echo":
+  command => "/bin/echo '$file_content'  >> $file_3",
+    onlyif  => "/usr/bin/test -f $file_3",
+    }
+    }
 #
-#if $mce_package == 'absent' {
-#  fail("Package is already absent. Unpin node from node group.")
-#}
 #
-#case $::osfamily {
-#  'Windows':{
-#    fail("Operating system not supported")
-#}
+#    #class mcelog {
+#    #$mce_package = mcelog
+#    #
+#    #if $mce_package == 'absent' {
+#    #  fail("Package is already absent. Unpin node from node group.")
+#    #}
+#    #
+#    #case $::osfamily {
+#    #  'Windows':{
+#    #    fail("Operating system not supported")
+#    #}
 # 'RedHat':{
 #package { 'mcelog':
 #  ensure => 'absent',
